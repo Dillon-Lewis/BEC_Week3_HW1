cf6ed93623d853697ce7a1cc092223e385d97d07
Dillon-Lewis <dilllewis42@gmail.com>
2024-06-11 16:33:55 -0400

    first commit

---
+++ 1_Tuple_Mastery.py
@@ -0,0 +1,20 @@
+# Task 1
+
+passengers = [('Alice', 'New York', 'London'), ("Bob", 'Tokyo', 'San Francisco'), ('Tim', 'Denver', 'Orlando')]
+# passengers = ("Alice", "Denver", 'Tokyo')
+
+#  Create a Python function that takes a list of tuples as an argument. 
+# Each tuple contains information about a flight itinerary: (traveler_name, origin, destination). 
+# The function should loop through the list of itineraries and print a formatted string for each.
+
+
+def loop(passengers):    
+        number = 1                                  #Needed to add the itinerary number
+        for data in passengers:                     #for all tuples in the list 
+            print(f'Itinerary: {number}', data[0],'- From', data[1], 'to', data[2])     #call upon each tuple by index
+
+            number += 1             # Adding an number to each statement after the next one
+
+loop(passengers)
+
+
---
+++ 2_Library_system.py
@@ -0,0 +1,46 @@
+#Task 1
+
+# You are maintaining a library system where each book is stored as a tuple within a list. Your task is to update this system by adding new books and ensuring no duplicates.
+
+#1. Add a new book to the files
+    #.append
+    #
+
+library = [("1984", "George Orwell"), ("Brave New World", "Aldous Huxley")]
+
+def main():
+    while True:
+        main = input('''
+Book List
+
+1. Add a book
+2. View book list
+3. Quit
+''')
+        if main == '1':
+            add()  # Add a book
+        elif main == '2':
+            number = 1               
+            for i in library:                    
+                    print(f'Book {number}:', i[0], "By ", i[1], )    
+                    number += 1   # View list
+        elif main == '3':
+             break
+        else:
+            print('enter correct number')
+            continue
+
+def add():
+        T1 = input("What is the Title of the book?\n").title()      
+        for title, author in library:
+             if T1 == title:
+                  print("Provide a book we dont already have!")
+                  return
+             
+        A1 = input('Who is the author of the book?\n') 
+        new_book = (T1, A1)
+        library.append(new_book)
+
+        
+                              
+main()
